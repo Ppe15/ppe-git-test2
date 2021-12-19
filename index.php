a83e6d2fdbb41cf4e563b39004af33dff84750b7
Fery Mauliadi Azis <ferymauliadiazis@gmail.com>
2021-12-19 17:53:31 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi adipisci officia ex impedit, fugit id itaque consectetur at quod repudiandae quasi quidem, eum possimus error porro ullam voluptates iste dolore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
